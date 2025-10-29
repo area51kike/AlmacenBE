54848eb7e872bdf93df937e82a19952d90fda171
betsaida <mangandibetsaida@gmail.com>
2025-10-28 21:19:20 -0600

    Test para los frm actuales

---
+++ src/test/java/sv/edu/ues/occ/ingenieria/prn335_2025/inventario/web/core/boundary/AlmacenFrmTest.java
@@ -0,0 +1,124 @@
+package sv.edu.ues.occ.ingenieria.prn335_2025.inventario.web.core.boundary;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlmacenFrmTest {
+
+    @Test
+    void inicializar() {
+    }
+
+    @Test
+    void inicializarRegistros() {
+    }
+
+    @Test
+    void selectionHandler() {
+    }
+
+    @Test
+    void btnNuevoHandler() {
+    }
+
+    @Test
+    void btnCancelarHandler() {
+    }
+
+    @Test
+    void btnGuardarHandler() {
+    }
+
+    @Test
+    void btnEliminarHandler() {
+    }
+
+    @Test
+    void btnModificarHandler() {
+    }
+
+    @Test
+    void esNombreVacio() {
+    }
+
+    @Test
+    void getEstado() {
+    }
+
+    @Test
+    void setEstado() {
+    }
+
+    @Test
+    void getNombreBean() {
+    }
+
+    @Test
+    void setNombreBean() {
+    }
+
+    @Test
+    void getRegistro() {
+    }
+
+    @Test
+    void setRegistro() {
+    }
+
+    @Test
+    void getModelo() {
+    }
+
+    @Test
+    void setModelo() {
+    }
+
+    @Test
+    void getPageSize() {
+    }
+
+    @Test
+    void setPageSize() {
+    }
+
+    @Test
+    void getFacesContext() {
+    }
+
+    @Test
+    void getDao() {
+    }
+
+    @Test
+    void nuevoRegistro() {
+    }
+
+    @Test
+    void buscarRegistroPorId() {
+    }
+
+    @Test
+    void getIdAsText() {
+    }
+
+    @Test
+    void getIdByText() {
+    }
+
+    @Test
+    void testEsNombreVacio() {
+    }
+
+    @Test
+    void testBtnGuardarHandler() {
+    }
+
+    @Test
+    void getListaTiposAlmacen() {
+    }
+
+    @Test
+    void setListaTiposAlmacen() {
+    }
+}
---
+++ src/test/java/sv/edu/ues/occ/ingenieria/prn335_2025/inventario/web/core/boundary/ProductoFrmTest.java
@@ -0,0 +1,112 @@
+package sv.edu.ues.occ.ingenieria.prn335_2025.inventario.web.core.boundary;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductoFrmTest {
+
+    @Test
+    void inicializar() {
+    }
+
+    @Test
+    void inicializarRegistros() {
+    }
+
+    @Test
+    void selectionHandler() {
+    }
+
+    @Test
+    void btnNuevoHandler() {
+    }
+
+    @Test
+    void btnCancelarHandler() {
+    }
+
+    @Test
+    void btnGuardarHandler() {
+    }
+
+    @Test
+    void btnEliminarHandler() {
+    }
+
+    @Test
+    void btnModificarHandler() {
+    }
+
+    @Test
+    void esNombreVacio() {
+    }
+
+    @Test
+    void getEstado() {
+    }
+
+    @Test
+    void setEstado() {
+    }
+
+    @Test
+    void getNombreBean() {
+    }
+
+    @Test
+    void setNombreBean() {
+    }
+
+    @Test
+    void getRegistro() {
+    }
+
+    @Test
+    void setRegistro() {
+    }
+
+    @Test
+    void getModelo() {
+    }
+
+    @Test
+    void setModelo() {
+    }
+
+    @Test
+    void getPageSize() {
+    }
+
+    @Test
+    void setPageSize() {
+    }
+
+    @Test
+    void getFacesContext() {
+    }
+
+    @Test
+    void getDao() {
+    }
+
+    @Test
+    void nuevoRegistro() {
+    }
+
+    @Test
+    void buscarRegistroPorId() {
+    }
+
+    @Test
+    void getIdAsText() {
+    }
+
+    @Test
+    void getIdByText() {
+    }
+
+    @Test
+    void testEsNombreVacio() {
+    }
+}
