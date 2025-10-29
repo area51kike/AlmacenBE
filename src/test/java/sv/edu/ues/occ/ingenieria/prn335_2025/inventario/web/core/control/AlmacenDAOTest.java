7f529ca86d89ab34340ef74d49f32919e973c65c
magokike <jacobodavid831@gmail.com>
2025-10-28 23:56:10 -0600

    AlmacenDAOTest

---
+++ src/test/java/sv/edu/ues/occ/ingenieria/prn335_2025/inventario/web/core/control/AlmacenDAOTest.java
@@ -0,0 +1,52 @@
+package sv.edu.ues.occ.ingenieria.prn335_2025.inventario.web.core.control;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlmacenDAOTest {
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findRange() {
+    }
+
+    @Test
+    void count() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void crear() {
+    }
+
+    @Test
+    void modificar() {
+    }
+
+    @Test
+    void eliminar() {
+    }
+
+    @Test
+    void eliminarPorId() {
+    }
+
+    @Test
+    void getEntityManager() {
+    }
+
+    @Test
+    void testFindById() {
+    }
+}
