9a6d709749dc6beb861065748c04ddb4af771bf8
jefferesquivel <AE24001@ues.edu.sv>
2025-10-28 23:58:31 -0600

    Agragar test Compradao

---
+++ src/test/java/sv/edu/ues/occ/ingenieria/prn335_2025/inventario/web/core/control/CompraDaoTest.java
@@ -0,0 +1,20 @@
+package sv.edu.ues.occ.ingenieria.prn335_2025.inventario.web.core.control;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompraDaoTest {
+
+    @Test
+    void getEntityManager() {
+    }
+
+    @Test
+    void crear() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
