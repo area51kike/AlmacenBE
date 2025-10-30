ca1a4c7b654af191778ae4860dfc79b54806d690
jefferesquivel <AE24001@ues.edu.sv>
2025-10-29 23:45:03 -0600

    test2

--- src/test/java/sv/edu/ues/occ/ingenieria/prn335_2025/inventario/web/core/control/ProductoTipoProductoDAOTest.java
+++ src/test/java/sv/edu/ues/occ/ingenieria/prn335_2025/inventario/web/core/control/ProductoTipoProductoDAOTest.java
@@ -1,16 +1,40 @@
 package sv.edu.ues.occ.ingenieria.prn335_2025.inventario.web.core.control;
 
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import sv.edu.ues.occ.ingenieria.prn335_2025.inventario.web.core.entity.ProductoTipoProducto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 class ProductoTipoProductoDAOTest {
 
+    @InjectMocks
+    ProductoTipoProductoDAO dao;
+
+    @Mock
+    EntityManager em;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     void getEntityManager() {
+        assertEquals(em, dao.getEntityManager());
     }
 
     @Test
     void findById() {
+        ProductoTipoProducto ptp = new ProductoTipoProducto();
+        when(em.find(ProductoTipoProducto.class, 1)).thenReturn(ptp);
+
+        ProductoTipoProducto result = dao.findById(1);
+        assertEquals(ptp, result);
     }
 }
