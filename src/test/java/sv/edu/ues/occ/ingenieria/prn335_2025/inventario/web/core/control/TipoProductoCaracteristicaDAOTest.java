978d513033dfa7e4c6c22d44196c4130b027dd84
jefferesquivel <AE24001@ues.edu.sv>
2025-10-29 23:38:14 -0600

    test1

--- src/test/java/sv/edu/ues/occ/ingenieria/prn335_2025/inventario/web/core/control/TipoProductoCaracteristicaDAOTest.java
+++ src/test/java/sv/edu/ues/occ/ingenieria/prn335_2025/inventario/web/core/control/TipoProductoCaracteristicaDAOTest.java
@@ -1,21 +1,40 @@
 package sv.edu.ues.occ.ingenieria.prn335_2025.inventario.web.core.control;
 
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import sv.edu.ues.occ.ingenieria.prn335_2025.inventario.web.core.entity.TipoProductoCaracteristica;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 class TipoProductoCaracteristicaDAOTest {
 
+    @InjectMocks
+    TipoProductoCaracteristicaDAO dao;
+
+    @Mock
+    EntityManager em;
+
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
     void getEntityManager() {
+        assertEquals(em, dao.getEntityManager());
     }
 
     @Test
     void findById() {
+        TipoProductoCaracteristica tpc = new TipoProductoCaracteristica();
+        when(em.find(TipoProductoCaracteristica.class, 1)).thenReturn(tpc);
+
+        TipoProductoCaracteristica result = dao.findById(1);
+        assertEquals(tpc, result);
     }
 }
--- src/test/java/sv/edu/ues/occ/ingenieria/prn335_2025/inventario/web/core/control/UnidadMedidaDAOTest.java
+++ src/test/java/sv/edu/ues/occ/ingenieria/prn335_2025/inventario/web/core/control/UnidadMedidaDAOTest.java
@@ -1,12 +1,30 @@
 package sv.edu.ues.occ.ingenieria.prn335_2025.inventario.web.core.control;
 
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import sv.edu.ues.occ.ingenieria.prn335_2025.inventario.web.core.entity.UnidadMedida;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UnidadMedidaDAOTest {
 
+    @InjectMocks
+    UnidadMedidaDAO dao;
+
+    @Mock
+    EntityManager em;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     void getEntityManager() {
+        assertEquals(em, dao.getEntityManager());
     }
 }
